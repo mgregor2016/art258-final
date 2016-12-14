25f41a6ec540e59fadfb4526e08fba87251154c5
mgregor2016 <nmastudent@KOAMAC005.local>
2016-12-13 20:24:15 -1000

    slide show tidy
    image tide, slide show tidy

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 // put all your JS in here
-$(document).ready(function(){
-$('.fade').slick({
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  speed: 2500,
-  fade: true,
-  cssEase: 'linear',
-  arrows: false
-});
-});
-	
-
 window.sr = ScrollReveal();
 sr.reveal('.foo');
 sr.reveal('.bar');
